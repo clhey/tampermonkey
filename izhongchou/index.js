0a520eb0698d8c8859325b3346536ddf63818838
酷卡移动 <office@cokapp.com>
2017-09-04 12:44:39 +0800

    link

--- izhongchou/index.js
+++ izhongchou/index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         抢淘宝众筹
-// @namespace    https://github.com/clhey/tampermonkey/izhongchou
+// @namespace    https://github.com/clhey/tampermonkey/tree/master/izhongchou
 // @version      0.1
 // @description  狗日的迅雷耍猴，玩客云抢了几天没抢到，没办法撸了个脚本半自动抢，果真抢到了。我不是黄牛撸1个就够，共享出来大家玩下。注意：我不是前端程序员，代码Low不许喷。
 // @author       查理
