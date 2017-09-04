0dd282626874b6b1dcad7d5c8a9e6b446f3bb585
酷卡移动 <dev@cokapp.com>
2017-09-04 12:01:00 +0800

    readme

--- izhongchou/index.js
+++ izhongchou/index.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         淘宝众筹
+// @name         抢淘宝众筹
 // @namespace    https://github.com/clhey/tampermonkey/izhongchou
 // @version      0.1
-// @description  狗日的迅雷耍猴，玩客云抢了几天没抢到，没办法撸了个脚本自动抢，果真抢到了。我不是黄牛撸1个就够，共享出来大家玩下。注意：我不是前端程序员，代码Low不许喷。
+// @description  狗日的迅雷耍猴，玩客云抢了几天没抢到，没办法撸了个脚本半自动抢，果真抢到了。我不是黄牛撸1个就够，共享出来大家玩下。注意：我不是前端程序员，代码Low不许喷。
 // @author       查理
 // @match        https://izhongchou.taobao.com/dreamdetail.htm?id=*
 // @require      http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
